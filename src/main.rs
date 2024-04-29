bf31e3ab7a3116b458f2e262ed80f801db70a46e
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-04-29 17:35:08 +0200

    try reference counting

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,38 @@
 use std::fs;
 use regex::Regex;
 use std::collections::HashSet;
-use std::{cell,RefCell, rc::Rc};
+use std::rc::Rc;
 
-struct Branch {
-    inbounds: 
+//use std::{cell::RefCell, rc::Rc};
+
+//pub type NodeRef = Rc<RefCell<Node>>;
+//pub type BranchRef = Rc<RefCell<Branch>>;
+
+#[derive(Default)]
+pub struct Branch {
+    inbounds: Option<Rc<Node>>,
+    outbounds: Option<Rc<Node>>,
 }
+
+#[derive(Default)]
+pub struct Node {
+    inbounds: Option<Rc<Branch>>,
+    left: Option<Rc<Branch>>,
+    right: Option<Rc<Branch>>,
+}
+
+impl Node{
+    pub fn set_left(&self, branch: Rc<Branch>) {
+        self.left = Some(branch);
+        //branch.inbounds = Some(&self);
+    }
+
+    pub fn set_right(&self, branch: Rc<Branch>) {
+       self.right = Some(branch);
+    }
+}
+
+
 
 fn tokenize(s: &str) -> Vec<String> {
     //let tokens: Vec<str> = Vec::new();
@@ -43,6 +70,9 @@ fn tokenize(s: &str) -> Vec<String> {
             }
         }
     }
+
+    let node = Node::default();
+
     return tokens;
 }
 
@@ -64,15 +94,30 @@ fn main() {
     //println!("With text: \n {stripped_contents}");
 
     let s = "(((A:0.5,B:0.5):0.5):1.0,C:1.5);";
-    //let v = tokenize(&stripped_contents);
     //println!("{}", v[0]);
-    //let v = tokenize(&s);
-    let v = tokenize(&stripped_contents);
+    let v = tokenize(&s);
+    //let v = tokenize(&stripped_contents);
 
     for i in v.iter(){
         let isp = i == ",";
         println!("{i} \t, is comma = {}", isp);
     }
+
+    let node = Rc::new(Node::default());
+    let branch_left = Rc::new(Branch::default());
+    let branch_right = Rc::new(Branch::default());
+
+    //node.left = Some(branch_left);
+    node.set_left(Rc::clone(&branch_left));
+    //
+    //let node_ref = &node;
+    //let branch_left_ref = &branch_left;
+
+    //node.left = Some(branch_left_ref);
+    //node.right = Some(branch_right);
+
+    //branch_left.inbounds = Some(node);
+
 
     /* let mut chars2 = s.chars();
     let mut iter2 = chars2.peekable();
