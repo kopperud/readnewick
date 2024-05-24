bbc3eeb03b70455520300c9f03d5b85eec4de983
kopperud <kopperud@protonmail.com>
2024-05-24 10:14:54 +0200

    bump

---
+++ src/linecount.rs
@@ -0,0 +1,22 @@
+use std::io;
+use std::io::{BufRead, BufReader};
+
+extern crate bytecount;
+
+pub fn count_lines<R: io::Read>(handle: R) -> Result<usize, io::Error> {
+    let mut reader = BufReader::with_capacity(1024*32, handle);
+    let mut count = 0;
+
+    loop {
+        let len = {
+            let buf = reader.fill_buf()?;
+            if buf.is_empty(){
+                break;
+            }
+            count += bytecount::count(&buf, b'\n');
+            buf.len()
+        };
+        reader.consume(len);
+    }
+    Ok(count)
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::collections::HashMap;
 use microbench::{self, Options};
 use bitvec::prelude::*;
 use indicatif::ProgressBar;
+use std::convert::TryFrom;
 
 
 use crate::parser::*;
@@ -12,6 +13,7 @@ use crate::utils::*;
 use crate::tokenizer::*;
 use crate::taxonlabels::*;
 use crate::splits::*;
+use crate::linecount::*;
 
 pub mod parser;
 pub mod utils;
@@ -19,101 +21,94 @@ pub mod tokenizer;
 pub mod tree;
 pub mod taxonlabels;
 pub mod splits;
+pub mod linecount;
 
 
+pub fn parse_tree(contents: String) -> Rc<Node> {
+    let newickstring = find_newick_string(contents);
+
+    let stripped_contents = stripcomments(&newickstring); 
+    let tokens = tokenize(&stripped_contents);
+    let root = parse_newick(&tokens);
+
+    return root
+}
 
 
 
 fn main() -> io::Result<()> {
-    //let contents = fs::read_to_string("primates.tre").expect("should have been able to readfile");
-    //let stripped_contents = stripcomments(&contents); 
-//    let stripped_contents = fs::read_to_string("ungulates.tre").expect("should have been able to readfile");
+
+    let args: Vec<String> = env::args().skip(1).collect();
+    dbg!(&args);
 
 
 
-    //println!("With text: \n {stripped_contents}");
+    let filenames = args;
 
-    let s = "((((A:0.5,B:0.5):1.5,C:1.5):0.5,(D:2,E:0.5):0.5):2.5,F:2.5);";
-    //let s = re.replace_all(string_with_comments, "");
-    //let s = stripped_contents;
- //   let tokens = tokenize(&stripped_contents);
-  //  let root = parse_newick(&tokens);
-   // let all_taxa = taxon_labels(&root);
-   // println!("taxon labels: \t {:?}", taxon_labels(&root));
+    // read first tree
+    // save the taxon names
+    // assume they are equal for all samples
+    let root = parse_tree(second_line);
+    let all_taxa = taxon_labels(&root);
 
-    //let n_tips = taxon_labels(&root).len();
- //   println!("number of taxa: {}", n_tips);
+    for filename in filenames{
+        let filename = "primates_cytb_JC_run_1.trees";
 
-   // let mut splits: Vec<BitVec> = Vec::new();
-  //  postorder_splits(&mut splits, &all_taxa, &root);
+        let file = File::open(&filename)?;
+        let n_lines = count_lines(&file).unwrap();
 
-    //for split in &splits{
-    //    println!("split: \t {:?}", &split);
-   // }
+        let file = File::open(&filename)?;
+        let f = BufReader::new(&file);
+        let count: u32 = n_lines as u32;
 
-    
+        let bar = ProgressBar::new(n_lines.try_into().unwrap());
 
-    let mut h: HashMap<BitVec, u64> = HashMap::new();
-    //for split in &splits{
-    //    *h.entry(split.clone()).or_insert(0) += 1;
-    //}
+        let lines = f.lines();
+        let mut h: HashMap<BitVec, u64> = HashMap::new();
+        let mut n_trees = 1.0;
+        
+        for (i, line) in lines.enumerate(){
+            if i > 0 {
+                let line_string = line.unwrap();
+                let root = parse_tree(line_string);
+                //let all_taxa = taxon_labels(&root);
+                //println!("all taxa: \t {:?}", &all_taxa);
 
+                // calculate the splits
+                let mut splits: Vec<BitVec> = Vec::new();
+                postorder_splits(&mut splits, &all_taxa, &root);
 
-    let f = File::open("primates_cytb_JC_run_1.trees")?;
-    let f = BufReader::new(f);
-
-    let mut n_trees: f64 = 0.0;
-
-    let bar = ProgressBar::new(5002);
-   
-    for (i, line) in f.lines().enumerate(){
-        if i > 0 {
-            let line_string = line.unwrap();
-            let newickstring = find_newick_string(line_string);
-
-            let stripped_contents = stripcomments(&newickstring); 
-            let tokens = tokenize(&stripped_contents);
-            let root = parse_newick(&tokens);
-            let all_taxa = taxon_labels(&root);
-            //println!("all taxa: \t {:?}", &all_taxa);
-
-            // calculate the splits
-            let mut splits: Vec<BitVec> = Vec::new();
-            postorder_splits(&mut splits, &all_taxa, &root);
-
-            // add the splits to the dictionary
-            for split in &splits{
-                *h.entry(split.clone()).or_insert(0) += 1;
+                // add the splits to the dictionary
+                for split in &splits{
+                    *h.entry(split.clone()).or_insert(0) += 1;
+                }
+                n_trees += 1.0;
+                bar.inc(1);
             }
-
-            //for split in splits{
-            //   println!("{:?}", split); 
-            //}
-            n_trees += 1.0;
-            bar.inc(1);
         }
-    }
-    bar.finish();
-     
-    println!("summary hash map: \t");
-    for (key, value) in &h{
-        println!("key: {:?}, \t val: {}", &key, &value);
-    }
+        bar.finish();
+         
+        //println!("summary hash map: \t");
+        //for (key, value) in &h{
+        //    println!("key: {:?}, \t val: {}", &key, &value);
+        //}
 
-    // calculate split frequencies
-    let mut split_frequencies: HashMap<BitVec, f64> = HashMap::new();
-    for (key, value) in h{
-        let split_occurrences = value as f64;
-        let split_frequency = split_occurrences / n_trees;
-        *split_frequencies.entry(key.clone()).or_insert(0.0) = split_frequency;
-    }
+        // calculate split frequencies
+        let mut split_frequencies: HashMap<BitVec, f64> = HashMap::new();
+        for (key, value) in h{
+            let split_occurrences = value as f64;
+            let split_frequency = split_occurrences / n_trees;
+            *split_frequencies.entry(key.clone()).or_insert(0.0) = split_frequency;
+        }
 
-    println!("split frequencies across posterior sample: \t");
-    println!("split \t frequency");
-    for (key, value) in &split_frequencies{
-        println!("{} \t {}", &key.to_string().replace(", ", ""), &value);
+        println!("split frequencies across posterior sample: \t");
+        println!("split \t frequency");
+        for (key, value) in &split_frequencies{
+            println!("{} \t {}", &key.to_string().replace(", ", ""), &value);
+        }
+
     }
-    //let options = Options::default();
+        //let options = Options::default();
     //microbench::bench(&options, "collect leaf labels", || taxon_labels(&root));
     Ok(())
 }
