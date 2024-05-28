565fb6bf1070c509630201e631d6f5357536e4e6
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-05-28 16:59:44 +0200

    simplify tokenizer, more fancy is not faster anyway

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,30 +1,31 @@
 
 pub fn tokenize(s: &str) -> Vec<&str> {
     let mut tokens: Vec<&str> = Vec::new();
+    const SPECIAL_TOKENS: &[char] = &['(', ')', ',', ';'];
+    let chars: Vec<char> = s.chars().collect();
 
-    const SPECIAL_TOKENS: [char; 4] = ['(', ')', ',', ';'];
     let mut start = 0;
+    let mut end = 0;
 
-    let mut iter = s.char_indices().peekable();
-
-    while let Some((end, c)) = iter.next(){
+    while end < chars.len(){
+        let c = chars[end];
         if SPECIAL_TOKENS.contains(&c){
             if start != end {
                 tokens.push(&s[start..end]);
             }
-            tokens.push(&s[end..end + c.len_utf8()]);
-            start = end + c.len_utf8();
-        }else if c == 0xA as char{
-            if start != end {
-                tokens.push(&s[start..end]);
+            if c != 0xA as char{
+                tokens.push(&s[end..end+1]);
             }
-            start = end + 1;
+            end += 1;
+            start = end;
+        }else{
+            end += 1;
         }
     }
 
-    if start < s.len(){
-        tokens.push(&s[start..s.len()]);
+    if start != end{
+        tokens.push(&s[start..end]);
     }
 
-    return tokens;
+    tokens
 }
