a3fcb845e3cb06311de7116eba4e06308e0d3b47
Bjorn Tore Kopperud <kopperud@protonmail.com>
2024-06-10 16:03:50 +0200

    dont call it root

--- src/taxonlabels.rs
+++ src/taxonlabels.rs
@@ -1,14 +1,21 @@
 use crate::tree::*;
 use std::rc::Rc;
 
-pub fn taxon_labels(root: &Rc<Node>) -> Vec<String> {
+pub fn taxon_labels(
+    node: &Rc<Node>
+    ) -> Vec<String> 
+{
     let mut taxa: Vec<String> = vec![];
 
-    taxon_labels_po(&mut taxa, root);
+    taxon_labels_po(&mut taxa, node);
     taxa
 }
 
-fn taxon_labels_po(taxa: &mut Vec<String>, node: &Rc<Node>){
+fn taxon_labels_po(
+    taxa: &mut Vec<String>, 
+    node: &Rc<Node>
+    )
+{
     let children = node.children.borrow();
 
     if children.is_empty(){
@@ -19,5 +26,4 @@ fn taxon_labels_po(taxa: &mut Vec<String>, node: &Rc<Node>){
         }
     }
 }
-
 
